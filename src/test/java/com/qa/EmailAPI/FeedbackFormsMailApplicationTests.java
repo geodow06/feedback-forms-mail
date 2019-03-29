1f58f78daff395c02cab471b81c89bbfea509775
JEuresQA <Jonathan.Eures@academytrainee.com>
2019-03-29 09:23:46 +0000

    Tried adding tests, they don't work

--- src/test/java/com/qa/EmailAPI/FeedbackFormsMailApplicationTests.java
+++ src/test/java/com/qa/EmailAPI/FeedbackFormsMailApplicationTests.java
@@ -1,9 +1,11 @@
-package com.qa;
+package com.qa.EmailAPI;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.qa.EmailAPI.service.EmailServiceImpl;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +13,12 @@ public class FeedbackFormsMailApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void sendsEmail() {
+		EmailServiceImpl service = new EmailServiceImpl();
+		service.sendSimpleMessage("Jonathan.Eures@academytrainee.com", "Test Email", "This is a test!");
 	}
 
 }
