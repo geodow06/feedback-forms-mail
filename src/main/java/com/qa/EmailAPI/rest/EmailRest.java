11541c42ec38384a7b2b9accc33cafb0e8df6858
JEuresQA <Jonathan.Eures@academytrainee.com>
2019-03-29 10:36:33 +0000

    Can send emails with Postman

--- src/main/java/com/qa/EmailAPI/rest/EmailRest.java
+++ src/main/java/com/qa/EmailAPI/rest/EmailRest.java
@@ -1,16 +1,25 @@
 package com.qa.EmailAPI.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.qa.EmailAPI.persistence.domain.Email;
 import com.qa.EmailAPI.service.EmailService;
 
 @CrossOrigin
 @RestController
 public class EmailRest {
 	
+	@Autowired
 	private EmailService src;
-	private RestTemplate rest;
+	
+	@PostMapping("/send")
+	public void sendSimpleMessage(@RequestBody Email email) {
+		src.sendSimpleMessage(email.getTo(), email.getSubject(), email.getText());
+	}
 
 }
